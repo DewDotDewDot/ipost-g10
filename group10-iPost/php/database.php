d9cb4f229ce378859264d20380d4012d1dcbcacd
DewDotDewDot <84194723+DewDotDewDot@users.noreply.github.com>
2021-05-14 23:52:17 +0800

    Delete group10-iPost directory

--- group10-iPost/php/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $source_db = "ipostdb";
-  $sql = new mysqli("localhost","root","",$source_db);
-  $sql_p = mysqli_connect("localhost","root","","finalsdb");
-  session_start();
-
-   $_SESSION["siteName"] = "IPOST";
-
-?>
