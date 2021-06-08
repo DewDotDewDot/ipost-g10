7cc924efaa2fa6af521b7ed2f6d4d01ab7282aa6
Dave <davezate01@gmail.com>
2021-06-08 10:51:58 +0800

    database refactoring successful. use main branch instead

--- group10-iPost/php/login.php
+++ group10-iPost/php/login.php
@@ -1,7 +1,7 @@
 <?php
-//data based accesed or connected 
+//data based accesed or connected
   include_once("database.php");
-  if (isset($_SESSION["user_type"])) 
+  if (isset($_SESSION["user_type"]))
   {
     header("location: index.php");
     exit();
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-     <title> <?php echo $_SESSION["siteName"] ?></title>
+     <title> <?php echo $_SESSION["siteName"] + "branch test" ?></title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
      <link rel="stylesheet" href="../css/form.css" type="text/css">
@@ -75,20 +75,20 @@
               <input type="text" name="username" required>
             </td>
           </tr>
-         
+
           <tr>
           	<td>Password</td>
             <td>
               <input type="password" name="password" required>
             </td>
           </tr>
-         
+
           <tr>
             <td colspan="2" style="text-align: center;">
             <a href="register.php"><p style="color:white"> Register Now!</p></a>
             </td>
           </tr>
-         
+
           <tr>
             <td colspan="2" style="text-align: center;">
               <div class="formItem">
