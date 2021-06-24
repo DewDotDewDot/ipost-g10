82868459b8b06271e24bc3a80bae7e7a4a507d82
Dave <davezate01@gmail.com>
2021-06-24 23:39:26 +0800

    Search User Function + Quick NavBar Fix.

--- group10-iPost/php/goodnavbar.php
+++ group10-iPost/php/goodnavbar.php
@@ -10,12 +10,20 @@ include_once("database.php");
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <?php if(ISSET($_SESSION['user_type'])) { ?>
         <li>
           <a class="nav-link active" aria-current="page" href="profile.php?id=<?php echo $_SESSION['user_id'] ?>">Profile</a>
         </li>
-        <li class="nav-item">
-         <a class="nav-link active" aria-current="page" href="logout.php">Log-out</a>
+       <li> <!-- Needs CSS. Search Bar for users-->
+         <form action="searchUser.php" method="post">
+           <input type="text" name="username">
+           <input type="submit" value="Find user">
+         </form>
        </li>
+       <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="logout.php">Log-out</a>
+      </li>
+      <?php } ?>
       </ul>
     </div>
   </div>
