7066c495f78c6bb7eae9ef805eab9074b84c10a3
ronron06 <ronniebughao@gmail.com>
2021-06-25 20:45:46 +0800

    bootstrap adminindex

--- group10-iPost/php/adminindex.php
+++ group10-iPost/php/adminindex.php
@@ -8,7 +8,8 @@ $_SESSION["user_type"] = 1;
 echo $_SESSION["user_name"];
 ?> </h1>
 
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <a href="register.php"><input value="Register an admin" type="button"></a>
 <a href="post.php"><input value="View posts" type="button"></a>
 <a href="userlist.php"><input value="View users" type="button"></a>
