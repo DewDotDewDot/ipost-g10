e1153adb6684fdd254a7790e6a6fef2305ce2277
ronron06 <ronniebughao@gmail.com>
2021-06-25 20:48:26 +0800

    as

--- group10-iPost/php/addUserAdmin.php
+++ group10-iPost/php/addUserAdmin.php
@@ -2,7 +2,7 @@
 <title> Add user admin </title>
 <body>
   <form action="register.php" method="post">
-  <h1> You are registering an
+  <h1> You are registering as
     <input type="text" name="userType" value="admin" readonly="true">
     account. Please confirm. <br>
   </h1>
